667a4467fa11caede011daf7119c37e9071c5336
Bruno Ponce <slohn73@localhost.localdomain>
2025-11-11 16:25:41 -0600

    ft:Se agrega el ejemplo de Eventos

--- js/script.js
+++ js/script.js
@@ -27,7 +27,30 @@ do{
     m++;
 } while(m<4);
 
+// Eventos
+
 const btnEstrella = document.getElementById("btnEstrella");
+
 btnEstrella.addEventListener("click", ()=> {
     cielo.innerHTML += "☆";
+})
+
+const btnPlaneta = document.getElementById("btnPlaneta");
+
+btnPlaneta.addEventListener("dblclick", ()=> {
+    cielo.innerHTML += "🪐";
+})
+
+const inputEmoji = document.getElementById("inputEmoji");
+
+const btnAgregarEmoji = document.getElementById("btnAgregarEmoji");
+
+btnAgregarEmoji.addEventListener("click", ()=> {
+    var emoji = inputEmoji.value.trim();
+    if(emoji) cielo.innerHTML += emoji;
+    inputEmoji.value="";
+})
+
+inputEmoji.addEventListener("keydown", e => {
+    if(e.key==="Escape") cielo.innerHTML="";
 })
